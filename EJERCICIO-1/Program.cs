1655366f449f527d0b851e49207d2b9c0b938f3e
alvaro1011 <alvaromurcia347@gmail.com>
2026-02-22 23:12:20 -0500

    Ee

---
+++ EJERCICIO-1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Ingrese el nombre:");
+string Nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese en nivel XP (1 - 100):");
+int XP = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Tienes suscripción premium? (si/no):");
+string Premium = Console.ReadLine();
+Boolean esPremium;
+
+if (Premium == "si")
+{
+    esPremium = true;
+}
+else
+{
+    esPremium = false;
+}
+
+if (esPremium == true)
+{
+    Console.WriteLine("Bienvenido " + Nombre + ", tu nivel de XP es: " + XP + " y tienes acceso a contenido exclusivo.");
+}
+else
+{
+    Console.WriteLine("Bienvenido " + Nombre + ", tu nivel de XP es: " + XP + " pero no tienes acceso a contenido exclusivo.");
+}
