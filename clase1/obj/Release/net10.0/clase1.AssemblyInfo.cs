600f4b5a14683baaa9c96c707ff3feb5b9204de8
alvaro1011 <alvaromurcia347@gmail.com>
2026-02-26 23:27:53 -0500

    22

--- clase1/obj/Release/net10.0/clase1.AssemblyInfo.cs
+++ clase1/obj/Release/net10.0/clase1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("clase1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a77d4a91e775809f3f653b7ceccee13edfc9eda6")]
 [assembly: System.Reflection.AssemblyProductAttribute("clase1")]
